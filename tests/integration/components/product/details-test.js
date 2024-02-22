9d76022a9299a7a44b6a44e32ff61dc306fd843f
Goutam112 <goutams112345@gmail.com>
2024-02-22 17:49:40 -0500

    explored testing with ember

--- tests/integration/components/product/details-test.js
+++ tests/integration/components/product/details-test.js
@@ -9,18 +9,7 @@ module('Integration | Component | product/details', function (hooks) {
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<Product::Details />`);
-
     assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Product::Details>
-        template block text
-      </Product::Details>
-    `);
-
-    assert.dom().hasText('template block text');
   });
 });
