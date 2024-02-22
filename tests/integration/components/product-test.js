737f3f73cfe3673e4d7aef5b70e85b4f5029b817
Goutam112 <goutams112345@gmail.com>
2024-02-22 18:19:05 -0500

    added unit test for products, added application test

--- tests/integration/components/product-test.js
+++ tests/integration/components/product-test.js
@@ -9,8 +9,29 @@ module('Integration | Component | product', function (hooks) {
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('product', {
+      id: '2',
+      name: 'Nike Aire Force 1',
+      description:
+        'Debuting in 1982, the AF1 was the first basketball shoe to house Nike Air, revolutionizing the game while rapidly gaining traction around the world.',
+      price: {
+        original: 109.95,
+        current: 89.98,
+      },
+      features: [
+        'Full-grain leather in the upper adds a premium look and feel.',
+        'Originally designed for performance hoops, Nike Air cushioning adds lightweight, all-day comfort.',
+        'The padded, low-cut collar looks sleek and feels great.',
+      ],
+      colors: [
+        {
+          color: 'white',
+          image: '/assets/images/nike-af1-white.png',
+        },
+      ],
+    });
 
-    await render(hbs`<Product />`);
-    assert.dom().hasText();
+    await render(hbs`<Product @product={{this.product}}/>`);
+    assert.dom('[data-test-product="2"]').isVisible();
   });
 });
